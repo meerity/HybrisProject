7dfb638b41abd511baa37474aec7dc13df78df17
meerity <antonychev10@gmail.com>
2024-10-31 15:49:59 +0200

    Created new accelerator

---
+++ training/trainingfulfilmentprocess/testsrc/com/epam/training/fulfilmentprocess/test/actions/PrepareOrderForManualCheck.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.epam.training.fulfilmentprocess.test.actions;
+
+/**
+ * Test counterpart for
+ * {@link com.epam.training.fulfilmentprocess.actions.order.PrepareOrderForManualCheckAction}
+ */
+public class PrepareOrderForManualCheck extends TestActionTemp
+{
+	//EMPTY
+}
---
+++ training/trainingfulfilmentprocess/testsrc/com/epam/training/fulfilmentprocess/test/actions/ScheduleForCleanUp.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.epam.training.fulfilmentprocess.test.actions;
+
+import com.epam.training.fulfilmentprocess.actions.order.ScheduleForCleanUpAction;
+
+
+/**
+ * Test counterpart for {@link ScheduleForCleanUpAction}
+ */
+public class ScheduleForCleanUp extends TestActionTemp
+{
+	//EMPTY
+}
