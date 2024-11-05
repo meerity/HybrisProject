55544a8360df2aec4d80a4f6ea3e5df1da66fc0e
meerity <antonychev10@gmail.com>
2024-11-05 23:54:57 +0200

    Completed ImpEx task

---
+++ trainingextension/src/com/epam/training/decorators/ProductCodeDecorator.java
@@ -0,0 +1,19 @@
+package com.epam.training.decorators;
+
+import de.hybris.platform.impex.jalo.header.AbstractImpExCSVCellDecorator;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+//makes string lowercase and without whitespaces
+public class ProductCodeDecorator extends AbstractImpExCSVCellDecorator {
+
+    @Override
+    public String decorate(int i, Map<Integer, String> map) {
+        String parsedValue = map.get(i);
+        if (parsedValue == null || parsedValue.isEmpty()){
+            return parsedValue;
+        }
+        return parsedValue.toLowerCase().replaceAll("\\s+", "");
+    }
+}
