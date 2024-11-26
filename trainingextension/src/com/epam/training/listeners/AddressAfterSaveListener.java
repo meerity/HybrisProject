1390cfbb27eb3b0c26adb345ee1afea8ccd25d03
meerity <antonychev10@gmail.com>
2024-11-26 16:55:32 +0200

    Completed ProcessEngine tasks 2 and 4 (Fixed)

--- trainingextension/src/com/epam/training/listeners/AddressAfterSaveListener.java
+++ trainingextension/src/com/epam/training/listeners/AddressAfterSaveListener.java
@@ -1,13 +1,12 @@
 package com.epam.training.listeners;
 
+import com.epam.training.model.AddressDefaultProcessModel;
 import de.hybris.platform.core.model.user.AddressModel;
 import de.hybris.platform.processengine.BusinessProcessService;
 import de.hybris.platform.servicelayer.event.impl.AbstractEventListener;
 import de.hybris.platform.servicelayer.event.events.AfterItemCreationEvent;
+import de.hybris.platform.servicelayer.model.ModelService;
 import org.apache.log4j.Logger;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AddressAfterSaveListener extends AbstractEventListener<AfterItemCreationEvent> {
 
@@ -15,32 +14,42 @@ public class AddressAfterSaveListener extends AbstractEventListener<AfterItemCre
     private static final String PROCESS_DEFINITION_NAME = "AddressDefaultProcess";
 
     private BusinessProcessService businessProcessService;
+    private ModelService modelService;
 
     @Override
     protected void onEvent(AfterItemCreationEvent event) {
-        if (event.getSource() instanceof AddressModel address) {
-            try {
-                launchAddressDefaultProcess(address);
-            } catch (Exception e) {
-                LOG.error("Failed to launch address process", e);
+        if (event != null && event.getSource() != null) {
+
+            final Object o = modelService.get(event.getSource());
+
+            if (o instanceof AddressModel address) {
+                try {
+                    launchAddressDefaultProcess(address);
+                } catch (Exception e) {
+                    LOG.error("Failed to launch address process", e);
+                }
             }
         }
     }
 
     private void launchAddressDefaultProcess(AddressModel address) {
         String processCode = "addressDefaultProcess-" + System.currentTimeMillis();
-        Map<String, Object> variables = new HashMap<>();
-        variables.put("address", address);
 
-        businessProcessService.startProcess(
+        AddressDefaultProcessModel process = businessProcessService.createProcess(
                 processCode,
-                PROCESS_DEFINITION_NAME,
-                variables
+                PROCESS_DEFINITION_NAME
         );
+        process.setAddress(address);
+
+        businessProcessService.startProcess(process);
     }
 
     public void setBusinessProcessService(BusinessProcessService businessProcessService) {
         this.businessProcessService = businessProcessService;
+    }
+
+    public void setModelService(ModelService modelService) {
+        this.modelService = modelService;
     }
 
 }
